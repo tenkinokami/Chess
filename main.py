63b72cd8dc1e2e9132c3032d099e9aa4a7796dbd
Adrien-Meilac <abonnements.ameilac@outlook.fr>
2018-11-13 11:34:51 +0100

    enumerate and score move

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ Created on Tue Nov 13 11:10:32 2018
 @author: AMEILAC
 """
 import piece as pc
+import plateau as pt
 import jeu
 import copy
 
 a = jeu.Jeu()
-print("a ..... :")
-print(a)
+#print("a ..... :")
+#print(a)
 L = a.actionsDisponibles(pc.blanc)
-b = copy.deepcopy(a)
-b.move(L[0])
-print("a ..... :")
-print(a)
-print("b ..... :")
-print(b)
+for action in L:
+    b = copy.deepcopy(a)
+    b.move(action)
+    M = b.actionsDisponibles(pc.noir)
+    for action2 in M:
+        c = copy.deepcopy(b)
+        c.move(action2)
+        (score_blanc, score_noir) = jeu.score1(c)
+        print(score_blanc, score_noir)
